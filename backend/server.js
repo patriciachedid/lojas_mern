ff13fbcc1e32df9e1486605493af026dd23b0da6
patriciachedid <patriciachedid@gmail.com>
2020-11-04 19:30:19 -0200

    inicializando back end

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const products = require('./data/products');
+const express = require("express");
+const products = require("./data/products");
 
 const app = express();
 
-app.listen(5000, console.log("servidor iniciado na porta 5000"));
-app.get( '/', (req, res) => {
-    req.send('a API esta funcionando')
+app.listen(5000, console.log("Servidor está rodando na porta 5000"));
 
+app.get("/", (req, res) => {
+  res.send("API está funcionando");
 });
 
-app.get('api/products', (req, res) => {
-    res.json(products)
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const id = req.params.id;
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(filteredProduct);
 });
